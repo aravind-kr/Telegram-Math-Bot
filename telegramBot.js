ac6573bca4c29ab29c17f12db5b518e83605e7a3
aravind-kr <arvindsmvec@gmail.com>
2019-01-30 20:06:52 +0530

    indentation corrected

--- telegramBot.js
+++ telegramBot.js
@@ -1,203 +1,225 @@
-"use strict";
+'use strict'
 
-const apiai = require("apiai");
-const uuid = require("node-uuid");
-const request = require("request");
+const apiai = require('apiai')
+const uuid = require('node-uuid')
+const request = require('request')
 
 module.exports = class TelegramBot {
-  get apiaiService() {
-    return this._apiaiService;
-  }
-
-  set apiaiService(value) {
-    this._apiaiService = value;
-  }
-
-  get botConfig() {
-    return this._botConfig;
-  }
-
-  set botConfig(value) {
-    this._botConfig = value;
-  }
-
-  get sessionIds() {
-    return this._sessionIds;
-  }
-
-  set sessionIds(value) {
-    this._sessionIds = value;
-  }
-
-  constructor(botConfig, baseUrl) {
-    this._botConfig = botConfig;
-    var apiaiOptions = {
-      language: botConfig.apiaiLang,
-      requestSource: "telegram"
-    };
-
-    this._apiaiService = apiai(botConfig.apiaiAccessToken, apiaiOptions);
-    this._sessionIds = new Map();
-
-    this._webhookUrl = baseUrl + "/webhook";
-    console.log("Starting bot on " + this._webhookUrl);
-
-    this._telegramApiUrl =
-      "https://api.telegram.org/bot" + botConfig.telegramToken;
-  }
-
-  start(responseCallback, errCallback) {
-    // https://core.telegram.org/bots/api#setwebhook
-    request.post(
-      this._telegramApiUrl + "/setWebhook",
-      {
-        json: {
-          url: this._webhookUrl
-        }
-      },
-      function(error, response, body) {
-        if (error) {
-          console.error("Error while /setWebhook", error);
-          if (errCallback) {
-            errCallback(error);
-          }
-          return;
-        }
-
-        if (response.statusCode != 200) {
-          console.error("Error status code while /setWebhook", body);
-          if (errCallback) {
-            errCallback("Error status code while setWebhook " + body);
-          }
-          return;
-        }
-
-        console.log("Method /setWebhook completed", body);
-        if (responseCallback) {
-          responseCallback("Method /setWebhook completed " + body);
-        }
-      }
-    );
-  }
-
-  processMessage(req, res) {
-    if (this._botConfig.devConfig) {
-      console.log("body", req.body);
+    get apiaiService() {
+        return this._apiaiService
     }
 
-    let updateObject = req.body;
+    set apiaiService(value) {
+        this._apiaiService = value
+    }
 
-    if (updateObject && updateObject.message) {
-      let msg = updateObject.message;
+    get botConfig() {
+        return this._botConfig
+    }
 
-      var chatId;
+    set botConfig(value) {
+        this._botConfig = value
+    }
 
-      if (msg.chat) {
-        chatId = msg.chat.id;
-      }
+    get sessionIds() {
+        return this._sessionIds
+    }
 
-      let messageText = msg.text;
+    set sessionIds(value) {
+        this._sessionIds = value
+    }
 
-      console.log(chatId, messageText);
-
-      if (chatId && messageText) {
-        if (!this._sessionIds.has(chatId)) {
-          this._sessionIds.set(chatId, uuid.v1());
+    constructor(botConfig, baseUrl) {
+        this._botConfig = botConfig
+        var apiaiOptions = {
+            language: botConfig.apiaiLang,
+            requestSource: 'telegram',
         }
 
-        let apiaiRequest = this._apiaiService.textRequest(messageText, {
-          sessionId: this._sessionIds.get(chatId)
-        });
+        this._apiaiService = apiai(botConfig.apiaiAccessToken, apiaiOptions)
+        this._sessionIds = new Map()
 
-        apiaiRequest.on("response", response => {
-          if (TelegramBot.isDefined(response.result)) {
-            let responseText = response.result.fulfillment.speech;
-            let responseData = response.result.fulfillment.data;
+        this._webhookUrl = baseUrl + '/webhook'
+        console.log('Starting bot on ' + this._webhookUrl)
 
-            if (
-              TelegramBot.isDefined(responseData) &&
-              TelegramBot.isDefined(responseData.telegram)
-            ) {
-              console.log("Response as formatted message");
+        this._telegramApiUrl =
+            'https://api.telegram.org/bot' + botConfig.telegramToken
+    }
 
-              let telegramMessage = responseData.telegram;
-              telegramMessage.chat_id = chatId;
+    start(responseCallback, errCallback) {
+        // https://core.telegram.org/bots/api#setwebhook
+        request.post(
+            this._telegramApiUrl + '/setWebhook',
+            {
+                json: {
+                    url: this._webhookUrl,
+                },
+            },
+            function(error, response, body) {
+                if (error) {
+                    console.error('Error while /setWebhook', error)
+                    if (errCallback) {
+                        errCallback(error)
+                    }
+                    return
+                }
 
-              this.reply(telegramMessage);
-              TelegramBot.createResponse(res, 200, "Message processed");
-            } else if (TelegramBot.isDefined(responseText)) {
-              console.log("Response as text message");
-              this.reply({
-                chat_id: chatId,
-                text: responseText
-              });
-              TelegramBot.createResponse(res, 200, "Message processed");
-            } else {
-              console.log("Received empty speech");
-              TelegramBot.createResponse(res, 200, "Received empty speech");
+                if (response.statusCode != 200) {
+                    console.error('Error status code while /setWebhook', body)
+                    if (errCallback) {
+                        errCallback(
+                            'Error status code while setWebhook ' + body
+                        )
+                    }
+                    return
+                }
+
+                console.log('Method /setWebhook completed', body)
+                if (responseCallback) {
+                    responseCallback('Method /setWebhook completed ' + body)
+                }
             }
-          } else {
-            console.log("Received empty result");
-            TelegramBot.createResponse(res, 200, "Received empty result");
-          }
-        });
-
-        apiaiRequest.on("error", error => {
-          console.error("Error while call to api.ai", error);
-          TelegramBot.createResponse(res, 200, "Error while call to api.ai");
-        });
-        apiaiRequest.end();
-      } else {
-        console.log("Empty message");
-        return TelegramBot.createResponse(res, 200, "Empty message");
-      }
-    } else {
-      console.log("Empty message");
-      return TelegramBot.createResponse(res, 200, "Empty message");
+        )
     }
-  }
 
-  reply(msg) {
-    // https://core.telegram.org/bots/api#sendmessage
-    request.post(
-      this._telegramApiUrl + "/sendMessage",
-      {
-        json: msg
-      },
-      function(error, response, body) {
-        if (error) {
-          console.error("Error while /sendMessage", error);
-          return;
+    processMessage(req, res) {
+        if (this._botConfig.devConfig) {
+            console.log('body', req.body)
         }
 
-        if (response.statusCode != 200) {
-          console.error("Error status code while /sendMessage", body);
-          return;
+        let updateObject = req.body
+
+        if (updateObject && updateObject.message) {
+            let msg = updateObject.message
+
+            var chatId
+
+            if (msg.chat) {
+                chatId = msg.chat.id
+            }
+
+            let messageText = msg.text
+
+            console.log(chatId, messageText)
+
+            if (chatId && messageText) {
+                if (!this._sessionIds.has(chatId)) {
+                    this._sessionIds.set(chatId, uuid.v1())
+                }
+
+                let apiaiRequest = this._apiaiService.textRequest(messageText, {
+                    sessionId: this._sessionIds.get(chatId),
+                })
+
+                apiaiRequest.on('response', response => {
+                    if (TelegramBot.isDefined(response.result)) {
+                        let responseText = response.result.fulfillment.speech
+                        let responseData = response.result.fulfillment.data
+
+                        if (
+                            TelegramBot.isDefined(responseData) &&
+                            TelegramBot.isDefined(responseData.telegram)
+                        ) {
+                            console.log('Response as formatted message')
+
+                            let telegramMessage = responseData.telegram
+                            telegramMessage.chat_id = chatId
+
+                            this.reply(telegramMessage)
+                            TelegramBot.createResponse(
+                                res,
+                                200,
+                                'Message processed'
+                            )
+                        } else if (TelegramBot.isDefined(responseText)) {
+                            console.log('Response as text message')
+                            this.reply({
+                                chat_id: chatId,
+                                text: responseText,
+                            })
+                            TelegramBot.createResponse(
+                                res,
+                                200,
+                                'Message processed'
+                            )
+                        } else {
+                            console.log('Received empty speech')
+                            TelegramBot.createResponse(
+                                res,
+                                200,
+                                'Received empty speech'
+                            )
+                        }
+                    } else {
+                        console.log('Received empty result')
+                        TelegramBot.createResponse(
+                            res,
+                            200,
+                            'Received empty result'
+                        )
+                    }
+                })
+
+                apiaiRequest.on('error', error => {
+                    console.error('Error while call to api.ai', error)
+                    TelegramBot.createResponse(
+                        res,
+                        200,
+                        'Error while call to api.ai'
+                    )
+                })
+                apiaiRequest.end()
+            } else {
+                console.log('Empty message')
+                return TelegramBot.createResponse(res, 200, 'Empty message')
+            }
+        } else {
+            console.log('Empty message')
+            return TelegramBot.createResponse(res, 200, 'Empty message')
+        }
+    }
+
+    reply(msg) {
+        // https://core.telegram.org/bots/api#sendmessage
+        request.post(
+            this._telegramApiUrl + '/sendMessage',
+            {
+                json: msg,
+            },
+            function(error, response, body) {
+                if (error) {
+                    console.error('Error while /sendMessage', error)
+                    return
+                }
+
+                if (response.statusCode != 200) {
+                    console.error('Error status code while /sendMessage', body)
+                    return
+                }
+
+                console.log('Method /sendMessage succeeded')
+            }
+        )
+    }
+
+    static createResponse(resp, code, message) {
+        return resp.status(code).json({
+            status: {
+                code: code,
+                message: message,
+            },
+        })
+    }
+
+    static isDefined(obj) {
+        if (typeof obj == 'undefined') {
+            return false
         }
 
-        console.log("Method /sendMessage succeeded");
-      }
-    );
-  }
+        if (!obj) {
+            return false
+        }
 
-  static createResponse(resp, code, message) {
-    return resp.status(code).json({
-      status: {
-        code: code,
-        message: message
-      }
-    });
-  }
-
-  static isDefined(obj) {
-    if (typeof obj == "undefined") {
-      return false;
+        return obj != null
     }
-
-    if (!obj) {
-      return false;
-    }
-
-    return obj != null;
-  }
-};
+}
