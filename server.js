c589bf3d4d472492259596cdb71bea9828f2a066
aravind-kr <arvindsmvec@gmail.com>
2019-01-30 19:54:54 +0530

    integrating dialogflow(api.ai)

--- server.js
+++ server.js
@@ -1,18 +1,62 @@
-const TelegramBot = require("node-telegram-bot-api");
-require('dotenv').config()
+"use strict";
 
-const TOKEN = process.env.BOT_TOKEN;
+require("dotenv").config();
 
-const bot = new TelegramBot(TOKEN, { polling: true });
+const apiai = require("apiai");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-bot.on("message", msg => {
-  var Hi = "hi";
-  if (
-    msg.text
-      .toString()
-      .toLowerCase()
-      .indexOf(Hi) === 0
-  ) {
-    bot.sendMessage(msg.chat.id, "Hello dear user");
+const TelegramBot = require("./telegramBot");
+const TelegramBotConfig = require("./telegramBotConfig");
+
+const REST_PORT = process.env.PORT || 5000;
+const DEV_CONFIG = process.env.DEVELOPMENT_CONFIG == "true";
+
+const APP_NAME = process.env.APP_NAME;
+const APIAI_ACCESS_TOKEN = process.env.APIAI_ACCESS_TOKEN;
+const APIAI_LANG = process.env.APIAI_LANG;
+const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN;
+
+var baseUrl = "";
+if (APP_NAME) {
+  // Heroku case
+  baseUrl = APP_NAME;
+}
+
+// console timestamps
+require("console-stamp")(console, "yyyy.mm.dd HH:MM:ss.l");
+
+const botConfig = new TelegramBotConfig(
+  APIAI_ACCESS_TOKEN,
+  APIAI_LANG,
+  TELEGRAM_TOKEN
+);
+
+botConfig.devConfig = DEV_CONFIG;
+
+const bot = new TelegramBot(botConfig, baseUrl);
+bot.start(
+  () => {
+    console.log("Bot started");
+  },
+  errStatus => {
+    console.error("It seems the TELEGRAM_TOKEN is wrong! Please fix it.");
   }
+);
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post("/webhook", (req, res) => {
+  console.log("POST webhook");
+
+  try {
+    bot.processMessage(req, res);
+  } catch (err) {
+    return res.status(400).send("Error while processing " + err.message);
+  }
+});
+
+app.listen(REST_PORT, function() {
+  console.log("Rest service ready on port " + REST_PORT);
 });
