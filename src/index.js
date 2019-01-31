4441563ea299f1bfab79ce730ee497f2e2eed86e
aravind-kr <arvindsmvec@gmail.com>
2019-01-31 18:12:09 +0530

    math bot added to the teleram bot flow

--- src/index.js
+++ src/index.js
@@ -47,96 +47,98 @@ function words_to_number_conversion(query) {
     return res_list.join(' ')
 }
 
-async function executeSkill(user_question, callback) {
-    //This should be dynamically loaded from some store
-    // var timeZone = 'UTC+05:30';
+function executeSkill(user_question) {
+    return new Promise((resolve, reject) => {
+        //This should be dynamically loaded from some store
+        // var timeZone = 'UTC+05:30';
 
-    //Change 'back' to 'ago' for the chrono library
-    user_question = user_question.replace(/back/g, 'ago')
-    user_question = user_question.replace(/-/g, ' - ')
-    user_question = user_question.replace(/\//g, ' / ')
+        //Change 'back' to 'ago' for the chrono library
+        user_question = user_question.replace(/back/g, 'ago')
+        user_question = user_question.replace(/-/g, ' - ')
+        user_question = user_question.replace(/\//g, ' / ')
 
-    // var qChrono = extractTimeAndDate(user_question);
+        // var qChrono = extractTimeAndDate(user_question);
 
-    var skillResult = {
-        status: false,
-        contexts: {},
-        answer: null,
-        expressive_answer: null,
-    }
-    try {
-        var user_question = user_question
-        console.log(user_question)
-        user_question = words_to_number_conversion(user_question)
-        console.log(user_question)
+        var skillResult = {
+            status: false,
+            contexts: {},
+            answer: null,
+            expressive_answer: null,
+        }
+        try {
+            var user_question = user_question
+            console.log(user_question)
+            user_question = words_to_number_conversion(user_question)
+            console.log(user_question)
 
-        var tokenize = winkTokenizer().tokenize
-        var tokens = tokenize(user_question)
+            var tokenize = winkTokenizer().tokenize
+            var tokens = tokenize(user_question)
 
-        let entity_result = NER.recognize(tokens)
-        // logger.debug(entity_result);
+            let entity_result = NER.recognize(tokens)
+            // logger.debug(entity_result);
 
-        let { global_op, eval_str, number_list } = _process(
-            entity_result,
-            logger
-        )
-
-        if (
-            number_list.length < 2 &&
-            !(
-                eval_str.includes('nthRoot') ||
-                eval_str.includes('cbrt') ||
-                eval_str.includes('sqrt') ||
-                eval_str.includes('factorial') ||
-                (global_op &&
-                    (global_op.uid == 'fact' ||
-                        global_op.uid == 'trig' ||
-                        global_op.uid == 'log' ||
-                        global_op.uid == 'percent' ||
-                        global_op.uid == 'power'))
+            let { global_op, eval_str, number_list } = _process(
+                entity_result,
+                logger
             )
-        )
-            throw 'cant perform that operation'
 
-        let ans = math.eval(eval_str)
+            if (
+                number_list.length < 2 &&
+                !(
+                    eval_str.includes('nthRoot') ||
+                    eval_str.includes('cbrt') ||
+                    eval_str.includes('sqrt') ||
+                    eval_str.includes('factorial') ||
+                    (global_op &&
+                        (global_op.uid == 'fact' ||
+                            global_op.uid == 'trig' ||
+                            global_op.uid == 'log' ||
+                            global_op.uid == 'percent' ||
+                            global_op.uid == 'power'))
+                )
+            )
+                throw 'cant perform that operation'
 
-        if (isFloat(ans)) {
-            ans = ans.toFixed(2)
+            let ans = math.eval(eval_str)
+
+            if (isFloat(ans)) {
+                ans = ans.toFixed(2)
+            }
+            skillResult.status = true
+            skillResult.answer = {
+                answer: ans,
+                operation_str: eval_str,
+                global_operator: global_op,
+                quickReply: [
+                    {
+                        action: 'copy',
+                        meta: ans + '',
+                        value: 'Copy',
+                        value_type: 'TEXT',
+                    },
+                    {
+                        action: 'smartkeyboard',
+                        meta: 'calculator',
+                        value: 'Open calculator',
+                        value_type: 'TEXT',
+                    },
+                    {
+                        action: 'tap',
+                        meta: 'What can you do? ',
+                        value: 'What can you do? ',
+                        value_type: 'TEXT',
+                    },
+                ],
+            }
+            skillResult.expressive_answer = ' ' + eval_str + ' is ' + ans + '.'
+            skillResult.android_ssml = skillResult.expressive_answer
+        } catch (err) {
+            logger.error(err)
         }
-        skillResult.status = true
-        skillResult.answer = {
-            answer: ans,
-            operation_str: eval_str,
-            global_operator: global_op,
-            quickReply: [
-                {
-                    action: 'copy',
-                    meta: ans + '',
-                    value: 'Copy',
-                    value_type: 'TEXT',
-                },
-                {
-                    action: 'smartkeyboard',
-                    meta: 'calculator',
-                    value: 'Open calculator',
-                    value_type: 'TEXT',
-                },
-                {
-                    action: 'tap',
-                    meta: 'What can you do? ',
-                    value: 'What can you do? ',
-                    value_type: 'TEXT',
-                },
-            ],
-        }
-        skillResult.expressive_answer = ' ' + eval_str + ' is ' + ans + '.'
-        skillResult.android_ssml = skillResult.expressive_answer
-    } catch (err) {
-        logger.error(err)
-    }
 
-    logger.info(JSON.stringify(skillResult))
-    callback(skillResult)
+        logger.info(JSON.stringify(skillResult))
+        resolve(skillResult)
+    })
 }
 
 module.exports = executeSkill
